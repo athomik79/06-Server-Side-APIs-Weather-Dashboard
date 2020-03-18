a75f0c08c0b0bc20e10ce4df4cb2f8cfb5b49f64
Andrew Thomas <athomik79@gmail.com>
2020-03-18 17:43:26 -0400

    updated html and styling, api is returning requested data

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-var cityNameDisplay = "My City";
-var cityCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + "city" + "&appid=785f12a440a00e93dd6c2b0cfa40dc38";
+// variables to append data
+var APIkey = "&appid=785f12a440a00e93dd6c2b0cfa40dc38";
+var cityName = "Miami"
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Miami&appid=785f12a440a00e93dd6c2b0cfa40dc38";
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
+// Functions for current weather
+function displayWeather () {
 
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + APIkey;
 
- $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=Washington%20DCo%20&appid=785f12a440a00e93dd6c2b0cfa40dc38",
+    $.ajax({
+    url: queryURL,
     method: "GET"
-  }).then(function(response) {
+    }).then(function(response) {
     console.log(response);
-  });
+    });
 
-  $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=New%20York%20&appid=785f12a440a00e93dd6c2b0cfa40dc38",
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
-
-  $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=San%20Francisco%20&appid=785f12a440a00e93dd6c2b0cfa40dc38",
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+}
+displayWeather();

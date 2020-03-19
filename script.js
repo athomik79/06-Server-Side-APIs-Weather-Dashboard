581232a71c821cf088aae32c36285d22e61b1dde
Andrew Thomas <athomik79@gmail.com>
2020-03-18 20:06:44 -0400

    city weather search retreiving information and dynamically adding to divs

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-// //define the varibles that we'll need to append data pulled from api to cards above---------------------------------//
-// var APIkey = "&appid=ecc0be5fd92206da3aa90cc41c13ca56";
-// var cityName = "Seattle"
-//var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=ecc0be5fd92206da3aa90cc41c13ca56";
-
-
-
 // Current city weather search function
 
 function searchCity(cityname) {
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
-    var queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=785f12a440a00e93dd6c2b0cfa40dc38";
+    var queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=785f12a440a00e93dd6c2b0cfa40dc38";
 
     $.ajax({
         url: queryURL,
@@ -44,9 +37,9 @@ function searchCity(cityname) {
         console.log(queryURLforcast);
         // Storing an array of results in the results variable
         var results = response.list;
-        //empty 5day div--------
+        // Empty 5day div
         $("#5day").empty();
-        //create HTML for 5day forcast...........................
+        // Create HTML for 5 day forecast
         for (var i = 0; i < results.length; i += 9) {
             // Creating a div
             var fiveDayDiv = $("<div class='card text-white bg-primary mx-auto' style='width: 8.5rem; height: 11rem;'>");
@@ -57,7 +50,7 @@ function searchCity(cityname) {
             console.log(date);
             console.log(temp);
             console.log(hum);
-            //creating a tags with the result items information
+            // Creating tags
             var h5date = $("<h5 class='card-title'>").text(date);
             var pTemp = $("<p class='card-text'>").text(temp);;
             var pHum = $("<p class='card-text'>").text(hum);;
@@ -83,11 +76,11 @@ $("#select-city").on("click", function (event) {
     // Store city name
     var cityInput = $("#city-input").val().trim();
 
-    //save search term to local storage.....
+    // Save to local storage
     var textContent = $(this).siblings("input").val();
     localStorage.setItem(cityInput, textContent);
 
-    // Running the searchCity function (passing in the city as an argument) 
+    // City search function 
     searchCity(cityInput);
 
 });
